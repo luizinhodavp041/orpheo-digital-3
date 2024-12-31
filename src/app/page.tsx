b355be94c662ff726788cbc39d1222ce5498951e
luizinhodavp041 <luizinhodavp041@gmail.com>
2024-12-31 00:02:37 -0300

    header

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import Header from "@/components/layout/header";
 import HeroSection from "@/components/sections/hero";
 import FeaturesSection from "@/components/sections/features";
 import ShowcaseSection from "@/components/sections/showcase";
@@ -10,6 +11,7 @@ import Footer from "@/components/layout/footer";
 export default function Home() {
   return (
     <main className="min-h-screen">
+      <Header />
       <HeroSection />
       <FeaturesSection />
       <ShowcaseSection />

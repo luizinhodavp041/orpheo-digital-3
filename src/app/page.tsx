38fad96eecd5dede9b8ab9152e538efbd02d2ada
luizinhodavp041 <luizinhodavp041@gmail.com>
2024-12-30 23:27:11 -0300

    features

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,13 @@
+"use client";
+
 import HeroSection from "@/components/sections/hero";
+import FeaturesSection from "@/components/sections/features";
 
 export default function Home() {
   return (
     <main className="min-h-screen">
       <HeroSection />
+      <FeaturesSection />
     </main>
   );
 }

e5c455b254c51983aae31466e42f1a97cae2fa1c
luizinhodavp041 <luizinhodavp041@gmail.com>
2025-01-06 21:06:18 -0300

    page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,8 +2,6 @@
 
 import Header from "@/components/layout/header";
 import HeroSection from "@/components/sections/hero";
-import FeaturesSection from "@/components/sections/features";
-import ShowcaseSection from "@/components/sections/showcase";
 import TestimonialsSection from "@/components/sections/testimonials";
 import CTASection from "@/components/sections/cta";
 import ServicesSection from "@/components/sections/services";
